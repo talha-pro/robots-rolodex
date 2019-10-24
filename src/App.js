a81699810835611f0650acdec86c87a1c2c97642
Talha <55407333+talha-pro@users.noreply.github.com>
2019-10-24 17:44:53 +0500

    'Added card list component'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import {CardList} from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      string: 'Aoa This is Talha ! And this is commited'
+      monsters: [
+        
+      ]
     };
   }
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users}));
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <p>{this.state.string}</p>
-          
-        </header>
+      <CardList name="Talha">
+      {
+        this.state.monsters.map(monster => (<h1 key= {monster.id}>{monster.name}</h1>))
+      }
+        
+      </CardList>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './card-list.style.css';
+export const CardList = (props) => {
+    return (<div className='card-list'>{props.children}</div>);
+}; 
