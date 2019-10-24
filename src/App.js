9d8c842267056579b7d5574b7948c82e32054015
Talha <55407333+talha-pro@users.noreply.github.com>
2019-10-24 13:34:57 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      string: 'Aoa This is Talha !'
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <p>{this.state.string}</p>
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
