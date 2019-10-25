0d23cf086a7b5f5fb28305d896cf34e832deb25c
talha-pro <55407333+talha-pro@users.noreply.github.com>
2019-10-25 17:43:12 +0500

    Updates

---
+++ precache-manifest.56ed4e929b1423e8cadfb154275a3a9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afec534cdf2f5c7f36de3973bc9c1be",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "b3fe5656c5e44ff37c73",
+    "url": "/robots-rolodex/static/css/main.780ac6e9.chunk.css"
+  },
+  {
+    "revision": "76a3c08e1d8878f5491d",
+    "url": "/robots-rolodex/static/js/2.d4fbdaa3.chunk.js"
+  },
+  {
+    "revision": "b3fe5656c5e44ff37c73",
+    "url": "/robots-rolodex/static/js/main.ea23a9dc.chunk.js"
+  },
+  {
+    "revision": "070c07fc0ebe46851735",
+    "url": "/robots-rolodex/static/js/runtime-main.a6fb5746.js"
+  }
+]);
